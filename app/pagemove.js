57dbef3a670e2b2d7810a6c93da8aad6d4ef35a2
abdellatifketoun <abdellatifketoun22@gmail.com>
2020-02-13 00:06:47 +0200

    appUpdate

--- app/pagemove.js
+++ app/pagemove.js
@@ -1,3 +1,7 @@
 document.getElementById('plus').onclick = function() {
 	document.getElementById('crash').style.display = 'block';
 }
+
+document.getElementById('close').onclick = function() {
+	document.getElementById('crash').style.display = 'none';
+}
