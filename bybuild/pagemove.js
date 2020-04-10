e50f1637b965ddb8d0f7479556623a83dd2f62b8
abdellatifketoun <abdellatifketoun22@gmail.com>
2020-04-10 19:15:58 +0300

    bybuild Site

---
+++ bybuild/pagemove.js
@@ -0,0 +1,14 @@
+
+var tit = document.getElementById("tit");
+
+
+
+setTimeout(function(){ 
+  myBtn.style.visibility = "visible";
+
+ }, 4500);
+
+ setTimeout(function(){ 
+  tit.style.visibility = "visible";
+
+ }, 300);
